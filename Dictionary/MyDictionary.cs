6e096f0a782f263121394f5916e55e8b53aac8b9
elif <deniz_yakamozu_1997@hotmail.com>
2021-01-23 01:03:55 +0300

    Proje dosyası ekle.

---
+++ Dictionary/MyDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dictionary
+{
+    class MyDictionary<T,P>
+    {
+        T[] Key;
+        P[] Value;
+
+        public MyDictionary()
+        {
+            Key = new T[0];
+            Value = new P[0];
+        }
+        public void add(T key,P value)
+        {
+            T[] tempKey = Key;
+            P[] tempValue = Value;
+            Key = new T[Key.Length + 1];
+            Value = new P[Value.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                Key[i] = tempKey[i];
+                Value[i] = tempValue[i];
+            }
+            Key[Key.Length - 1] = key;
+            Value[Value.Length - 1] = value;
+          
+
+
+        }
+
+    }
+}
---
+++ Dictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> Ogrenci = new Dictionary<int, string>();//yapı bu
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.add(1903, "Besiktas");
+            myDictionary.add(1907, "Fenerbahce");
+
+            
+            
+        }
+    }
+}
